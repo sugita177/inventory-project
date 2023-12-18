093eaa77ae5522d87b585c3b7d618253640a1349
sugita177 <moriaki.sugita.1730@gmail.com>
2023-12-18 20:41:47 +0900

    データベース追加

--- database/migrations/2023_12_18_195012_create_places_table.php
+++ database/migrations/2023_12_18_195012_create_places_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('articles', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');;
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->timestamps();
         });
     }
 
@@ -21,8 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('articles', function (Blueprint $table) {
-            $table->dropColumn('user_id');
-        });
+        Schema::dropIfExists('places');
     }
 };
