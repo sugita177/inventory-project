6729af1138fb6f00e005e974ce35189e58fc88d9
sugita177 <moriaki.sugita.1730@gmail.com>
2023-12-19 18:01:34 +0900

    品目の詳細画面のレイアウトを変更

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,142 +4,186 @@
         在庫管理システム
         </h2>
     </x-slot>
-<!-- FAQ -->
-<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-  <!-- Title -->
-  <div class="max-w-2xl mx-auto mb-10 lg:mb-14">
-    <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">個別表示</h2>
+
+  
+
+  <div class="max-w-[60rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Card -->
+  <div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+      <div class="p-1.5 min-w-full inline-block align-middle">
+        <div class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
+          <!-- Header -->
+          <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-gray-700">
+            <div>
+              <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                {{ $article->name }}
+              </h2>
+              <p class="text-sm text-gray-600 dark:text-gray-400">
+                在庫対象品目の詳細
+              </p>
+            </div>
+
+            <div>
+              <div class="inline-flex gap-x-2">
+                <a href="{{route('article.edit', $article)}}" class="flex-1">
+                  <x-primary-button>編集</x-primary-button>
+                </a>
+
+                <form method="post" action="{{route('article.destroy', $article)}}" class="flex-2">
+                    @csrf
+                    @method('delete')
+                    <x-primary-button class="bg-red-700 ml-2">削除</x-primary-button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- End Header -->
+
+
+          <!-- Table -->
+          <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+            <thead class="bg-gray-50 divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left border-l border-gray-200 dark:border-gray-700">
+                  <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                    項目
+                  </span>
+                </th>
+
+                <th scope="col" class="px-6 py-3 text-left">
+                  <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                    内容
+                  </span>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">品名</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->name }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">細目</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->detail }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">分類</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->category->name }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">場所</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->place->name }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">管理単位</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->unit }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">発注先</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->supplier->name }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">備考</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->remark }}</span>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">登録日</span>
+                        </div>
+                    </td>
+                    <td class="h-px w-auto whitespace-nowrap">
+                        <div class="px-6 py-2">
+                            <span class="font-semibold text-sm text-gray-800 dark:text-gray-200">{{ $article->created_at }}</span>
+                        </div>
+                    </td>
+                </tr>
+                
+              
+            </tbody>
+          </table>
+          <!-- End Table -->
+
+          <!-- Footer -->
+          <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-gray-700">
+            
+
+            
+            
+          </div>
+          <!-- End Footer -->
+        </div>
+      </div>
+    </div>
   </div>
-  <!-- End Title -->
-
-  <div class="text-right flex">
-    <a href="{{route('article.edit', $article)}}" class="flex-1">
-        <x-primary-button>編集</x-primary-button>
-    </a>
-
-    <form method="post" action="{{route('article.destroy', $article)}}" class="flex-2">
-        @csrf
-        @method('delete')
-        <x-primary-button class="bg-red-700 ml-2">削除</x-primary-button>
-    </form>
-  </div>
-
-  <div class="max-w-2xl mx-auto divide-y divide-gray-200 dark:divide-gray-700">
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            品名
-          </h3>
-          <p class="mt-1 text-gray-500">
-            {{$article->name}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            細目
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->detail}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            分類
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->category->name}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            場所
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->place->name}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            管理単位
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->unit}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            発注先
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->supplier->name}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            備考
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->remark}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="py-8 first:pt-0 last:pb-0">
-      <div class="flex gap-x-5">
-
-        <div>
-          <h3 class="md:text-lg font-semibold text-gray-800 dark:text-gray-200">
-            登録日
-          </h3>
-          <p class="mt-1 text-gray-500">
-          {{$article->created_at}}
-          </p>
-        </div>
-      </div>
-    </div>
-
-
-  </div>
+  <!-- End Card -->
 </div>
-<!-- End FAQ -->
+<!-- End Table Section -->
 
 </x-app-layout>
