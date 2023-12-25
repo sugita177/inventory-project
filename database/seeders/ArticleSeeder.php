4ab9a598b5eb3ef22a89390fdd6037cef8e15c1f
sugita177 <moriaki.sugita.1730@gmail.com>
2023-12-25 13:47:11 +0900

    seedのcreated_atを修正

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use \App\Models\Article;
+use DateTime;
 
 class ArticleSeeder extends Seeder
 {
@@ -22,7 +23,8 @@ class ArticleSeeder extends Seeder
             'unit' => '缶',
             'supplier_id' => 2,
             'remark' =>'1/4以下になったら購入すること',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => 'ペンキ白',
@@ -32,7 +34,8 @@ class ArticleSeeder extends Seeder
             'unit' => '缶',
             'supplier_id' => 2,
             'remark' =>'1/4以下になったら購入すること',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => 'ペンキ黄',
@@ -42,7 +45,8 @@ class ArticleSeeder extends Seeder
             'unit' => '缶',
             'supplier_id' => 2,
             'remark' =>'1/4以下になったら購入すること',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => 'ペンキ用はけ',
@@ -52,7 +56,8 @@ class ArticleSeeder extends Seeder
             'unit' => '本',
             'supplier_id' => 2,
             'remark' =>'新品は5本以上ストックしておく',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => '電気配線',
@@ -62,7 +67,8 @@ class ArticleSeeder extends Seeder
             'unit' => '巻',
             'supplier_id' => 3,
             'remark' =>'半分以下になったら購入する',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => '電気配線',
@@ -72,7 +78,8 @@ class ArticleSeeder extends Seeder
             'unit' => '巻',
             'supplier_id' => 3,
             'remark' =>'半分以下になったら購入する',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => 'サンダー本体',
@@ -82,7 +89,8 @@ class ArticleSeeder extends Seeder
             'unit' => '個',
             'supplier_id' => 4,
             'remark' => 'メーカー注意',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => 'サンダー替刃',
@@ -92,7 +100,8 @@ class ArticleSeeder extends Seeder
             'unit' => '箱',
             'supplier_id' => 4,
             'remark' => '',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => 'バフ',
@@ -102,7 +111,8 @@ class ArticleSeeder extends Seeder
             'unit' => '箱',
             'supplier_id' => 4,
             'remark' => '',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => '溶接用ガス',
@@ -112,7 +122,8 @@ class ArticleSeeder extends Seeder
             'unit' => '本',
             'supplier_id' => 5,
             'remark' => '半分以下になったら注文すること',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
         Article::create([
             'name' => '溶接用ガス',
@@ -122,7 +133,8 @@ class ArticleSeeder extends Seeder
             'unit' => '本',
             'supplier_id' => 5,
             'remark' => '予備用に満タン1本はストックしておく',
-            'user_id' => rand(2, 4)
+            'user_id' => rand(2, 4),
+            'created_at' => new DateTime('2023-04-01 15:30')
         ]);
     }
 }
